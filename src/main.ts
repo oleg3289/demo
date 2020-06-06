a80b365eb75878eabf89b4a4a52b7a8b0775ff4f
Sytnik Oleg <olegsitnik2011@gmail.com>
2020-06-06 19:45:59 +0300

    material update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'hammerjs';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule);
